a6f47b5b82c88c77a6fe574bd5cbf6cd827a7ac0
human <human@neet.fi>
2018-09-30 21:22:40 +0000

    [2018-10-01] Modify src/misc/splitPath.ts

--- src/misc/splitPath.ts
+++ src/misc/splitPath.ts
@@ -1,5 +1,8 @@
 import * as path from "path";
 
+/**
+ * splits a path into an array of components
+ */
 export const splitPath = (s: string) => {
 	return s.split(path.sep).filter((c) => c !== "");
 };
