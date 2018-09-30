a3731502d2e3232d1863bd55edf420c23900d5fc
human <human@neet.fi>
2018-09-30 21:23:23 +0000

    [2018-10-01] Modify src/misc/charsMatching.ts

--- src/misc/charsMatching.ts
+++ src/misc/charsMatching.ts
@@ -1,3 +1,6 @@
+/**
+ * returns an array of single characters matching the given regular expression
+ */
 export const charsMatching = (re: RegExp) => {
 	const result = [];
 	for (let i = 0; i <= 65535; i++) {
