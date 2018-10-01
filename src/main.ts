005d9527518385deb591992c84a53bc060f807eb
human <human@neet.fi>
2018-10-01 02:05:41 +0000

    [2018-10-01] Modify src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,38 +5,25 @@ import * as util from "util";
 const mkdirpAsync = util.promisify(mkdirp);
 
 import {Dir} from "./misc/fs/Dir"
-import {File} from "./misc/fs/File"
+import {DynamicFile} from "./misc/fs/DynamicFile"
 import {splitPath} from "./misc/splitPath";
-import {BindSystem} from "./misc/binds/BindSystem";
-import {applyBinds} from "./binds";
+import {Game} from "./Game";
 
-const PROD = true;
+const PROD = false;
 
 const mountpoint =
 	PROD
 	? "/home/human/.steam/steam/steamapps/common/Team Fortress 2/tf/custom/cfgfs"
 	: "/home/human/test";
 
-const bindsys = new BindSystem();
-applyBinds(bindsys);
-
 const root = new Dir(Object.entries({
-	cfg: new Dir(Object.entries({
-		binds: bindsys.dir,
-		"autoexec.cfg": new File(() => {
-			// todo: remove once read and exec the real one
-			return (
-				`echo "hi from cfgfs autoexec"\n` +
-				`${bindsys.getInitCommands()}\n` +
-				""
-			);
-		}),
-	})),
 	r: (() => {
 		let n = 0;
-		return new File(() => `i've been accessed ${n++} times\n`);
+		return new DynamicFile(() => `i've been accessed ${n++} times\n`);
 	})(),
 }));
+
+const game = new Game(root);
 
 (async () => {
 
@@ -45,24 +32,43 @@ await mkdirpAsync(mountpoint);
 fuse.mount(mountpoint, {
 	options: ["direct_io"],
 	readdir: (path, cb) => {
-		console.log("readdir %s", path);
 		const node = root.lookupPath(splitPath(path));
 		if (node === null) {
-			cb(0);
+			cb(fuse.ENOENT);
 			return;
 		}
 		if (node instanceof Dir) {
-			cb(0, node.getContents());
+			cb(0, [
+				".",
+				"..",
+				...node.getContents(),
+			]);
 			return;
 		}
-		if (node instanceof File) {
-			cb(0);
+		if (node instanceof DynamicFile) {
+			cb(fuse.ENOTDIR);
 			return;
+		}
+		cb(fuse.EPERM);
+	},
+	open: (path, _flags, cb) => {
+		console.log("open(%s)", JSON.stringify(path));
+		const m = /^\/cfg\/binds\/([+-])([^]+)\.cfg$/.exec(path);
+		if (m !== null) {
+			game.key.dispatch({key: m[2], down: m[1] === "+"});
+		}
+		cb(fuse.ENOENT);
+	},
+	release: (path, _flags, cb) => {
+		console.log("release(%s)", JSON.stringify(path));
+		const node = root.lookupPath(splitPath(path));
+		if (node !== null) {
+			node.emit("release");
 		}
 		cb(0);
 	},
 	getattr: function (path, cb) {
-		console.log("getattr %s", path);
+		console.log("getattr(%s)", JSON.stringify(path));
 		const node = root.lookupPath(splitPath(path));
 		if (node === null) {
 			cb(fuse.ENOENT);
@@ -73,7 +79,6 @@ fuse.mount(mountpoint, {
 				mtime: new Date(),
 				atime: new Date(),
 				ctime: new Date(),
-				nlink: 1,
 				size: node.getContents().length,
 				mode: 16877,
 				uid: process.getuid(),
@@ -81,53 +86,57 @@ fuse.mount(mountpoint, {
 			});
 			return;
 		}
-		if (node instanceof File) {
-			console.log("size of %s before get: %s", path, node.getContent().length);
-			node.updateContent();
-			const size = node.getContent().length;
-			console.log("size of %s after get: %s", path, size);
+		if (node instanceof DynamicFile) {
 			cb(0, {
 				mtime: new Date(),
 				atime: new Date(),
 				ctime: new Date(),
-				nlink: 1,
-				size,
+				/*
+				size: (() => {
+					console.log("getattr: size of %s before get: %s", path, node.getContent().length);
+					node.updateContent();
+					const size = node.getContent().length;
+					console.log("getattr: size of %s after get: %s", path, size);
+					return size;
+				})(),
+				*/
+				size: 0,
 				mode: 33188,
 				uid: process.getuid(),
 				gid: process.getgid(),
-			})
+			});
 			return;
 		}
 		cb(fuse.ENOENT);
 	},
-	open: (path, _flags, cb) => {
-		console.log("open %s", path);
-		cb(0, 42);
-	},
-	read: (path, _fd, buf, len, pos, cb) => {
-		console.log("read %s", path);
+	read: (path, _fd, buf, length, position, cb) => {
+		console.log("read(%s)", JSON.stringify(path));
+		console.log("read: wants bytes %d-%d (%d)", position, position + length, length);
 		const node = root.lookupPath(splitPath(path));
 		if (node === null) {
-			cb(0);
+			console.log("read: file doesn't exist");
+			cb(fuse.ENOENT);
 			return;
 		}
 		if (node instanceof Dir) {
-			cb(0);
+			cb(fuse.EISDIR);
 			return;
 		}
-		if (node instanceof File) {
-			const content = node.getContent();
-			const slice = content.slice(pos, pos + len);
+		if (node instanceof DynamicFile) {
+			const slice = node.getContent().slice(position, position + length);
+			const written = slice.length;
+			console.log("read: wrote bytes %d-%d (%d)", position, position + written, written);
 			buf.write(slice);
-			cb(slice.length);
+			cb(written);
 			return;
 		}
-		cb(0);
+		cb(fuse.EPERM);
 	},
 }, (err) => {
 	if (err) {
 		throw err;
 	}
+	console.log("mounted cfgfs at %s", mountpoint);
 });
 
 process.on("SIGINT", () => {
